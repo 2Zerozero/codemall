36c2ba1adce3355c3e67e781168dbd4826b52bfc
2Zerozero <hawgae@gmail.com>
2025-02-17 04:15:55 +0900

    [feat] Head 적용용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import type { AppProps } from 'next/app';
 import '@/styles/global.css';
+import Head from 'next/head';
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Codeit Mall</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
